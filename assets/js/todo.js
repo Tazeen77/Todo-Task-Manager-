40258058af76aff40a904fdbebf615e6a1589f9f
Tazeen Khan <59621766+Tazeen77@users.noreply.github.com>
2020-03-21 08:15:21 +0530

    Add files via upload

---
+++ assets/js/todo.js
@@ -0,0 +1,27 @@
+//Check Off Specifc Todos By Clicking
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+	
+});
+
+
+//Click on X to delete Todo
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	//jquery method to stop bubbling as span is the part of li
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which == 13){
+			//grabbing new todo text from the input
+			var todoText = $(this).val();
+			//create a new li and add to ul
+			$(this).val("");
+			$('ul').append("<li><span> <i class='fa fa-trash'></i> </span>"+todoText+"</li>")
+		}
+
+});
+
